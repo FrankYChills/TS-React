858d57c9841173a7ef6b58e4a4c7dd939d2090d2
franky <itskchills11@gmail.com>
2023-03-14 15:02:59 +0530

    useReducer done

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,10 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
-
-interface User {
-  id: number;
-  user: string;
-}
-
-type fibFunc = (n: number) => number;
-
-const fib: fibFunc = (n) => {
-  if (n < 2) {
-    return n;
-  } else {
-    return fib(n - 1) + fib(n - 2);
-  }
-};
-
-const myNum: number = 30;
+import Counter from "./Counter";
 
 function App() {
-  const [count, setCount] = useState<number>(0);
-  const [users, setUsers] = useState<User[] | null>(null);
-
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  console.log("referenced element: ", inputRef.current);
-  console.log("element's value: ", inputRef.current?.value);
-
-  useEffect(() => {
-    console.log("Mounting App Component");
-    console.log("Users: ", users);
-
-    return () => console.log("Triggered cause App Component is unmounting");
-  }, [users]);
-
-  // Memoized function -> it saves the return for particular input in cache if that input is passed again/or page is refreshed else for other input it calculates the return
-  const addTwo = useCallback((): void => setCount((prev) => prev + 2), []);
-
-  const result = useMemo<number>(() => fib(myNum), [myNum]);
-
   return (
     <>
-      <h1>React Hooks</h1>
-      <h1>{count}</h1>
-      <button onClick={addTwo}>Add</button>
-      <h4>
-        Fibnocci of {myNum} is {result}
-      </h4>
-      <input ref={inputRef} type="text" />
+      <h1>React useReducer</h1>
+      <Counter>{(num: number) => <>Current Count : {num}</>}</Counter>
     </>
   );
 }
